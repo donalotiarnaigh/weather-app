cb3e899e6794d2040ddef877b0c1fc2b2dfa2291
dtcurragh <97108312+dtcurragh@users.noreply.github.com>
2022-05-23 16:04:41 +0100

    Edit app.js to allow servr run on Heroku. Add Procfile.

--- app.js
+++ app.js
@@ -45,6 +45,6 @@ app.post("/", function(req, res) {
 
 //Run server//
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Server running on port 3000");
 });
